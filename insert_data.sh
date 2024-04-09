e7b3ca67c60d690931965635fa541fa405a88b66
Sasa Filipovic <ojadekses@gmail.com>
2024-04-09 23:25:48 +0200

    Completed insert_data and queries scripts

--- insert_data.sh
+++ insert_data.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-if [[ $1 == "test" ]]
-then
-  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
-else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-fi
 
-# Do not change code above this line. Use the PSQL variable above to query your database.
+PSQL="psql -X --username=freecodecamp --dbname=worldcup -t --no-align -c"
 
 #year,round,winner,opponent,winner_goals,opponent_goals
 echo $($PSQL "TRUNCATE teams, games")
