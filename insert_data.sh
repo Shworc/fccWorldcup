b5e4bb9b0c46340b65fd53918fa4afdd19639844
Sasa Filipovic <ojadekses@gmail.com>
2024-04-10 00:29:12 +0200

    Corrected queries.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#! /bin/bash
 
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
 
-PSQL="psql -X --username=freecodecamp --dbname=worldcup -t --no-align -c"
-
-#year,round,winner,opponent,winner_goals,opponent_goals
+# Do not change code above this line. Use the PSQL variable above to query your database.
 echo $($PSQL "TRUNCATE teams, games")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
@@ -15,7 +19,7 @@ do
     # if not found
     if [[ -z $WINNER_ID ]]
     then
-      $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      INSERT0=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
       WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
     fi
     
@@ -23,19 +27,11 @@ do
     # if not found
     if [[ -z $OPPONENT_ID ]]
     then
-      $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      INSERT1=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
     fi
-      
-      
-      
-      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
+     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
-	  $($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+	  INSERT2=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
-done
-
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
-
 done
